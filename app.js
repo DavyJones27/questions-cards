16594387067e513c0bcbc4d31195c634138cf377
DavyJones <ajayyadavkol16@gmail.com>
2020-08-31 16:31:50 +0530

    all api fix except delete module

--- app.js
+++ app.js
@@ -28,27 +28,27 @@ let numOfOptions = 2;
 let questionnum = -1;
 function eventxListener() {
   const ui = new UI();
-  closeBtnQform.addEventListener("click", function (e) {
+  closeBtnQform.addEventListener("click", function(e) {
     ui.hideQuestion(questioncard);
   });
-  closeBtnMform.addEventListener("click", function (e) {
+  closeBtnMform.addEventListener("click", function(e) {
     ui.hideQuestion(modulecard);
   });
-  showbtn[0].addEventListener("click", function () {
+  showbtn[0].addEventListener("click", function() {
     ui.showQuestion(modulecard);
   });
-  addOptions.addEventListener("click", function (e) {
+  addOptions.addEventListener("click", function(e) {
     e.preventDefault();
     numOfOptions++;
     ui.addOption(optionAreaQform, numOfOptions);
   });
-  removeOption.addEventListener("click", function (e) {
+  removeOption.addEventListener("click", function(e) {
     e.preventDefault();
     ui.deleteOption(numOfOptions);
     numOfOptions--;
   });
 
-  formModule.addEventListener("submit", async function (e) {
+  formModule.addEventListener("submit", async function(e) {
     e.preventDefault();
     const moduleDetails = {};
     let name, value;
@@ -61,6 +61,7 @@ function eventxListener() {
       }
       moduleDetails[name] = value;
     }
+
     if (check) {
       const message = await sendData(
         "http://ec2-13-232-39-98.ap-south-1.compute.amazonaws.com:3000/admin/courses", //Create Module endpoint POST
@@ -76,7 +77,7 @@ function eventxListener() {
     }
     check = true;
   });
-  form.addEventListener("submit", (e) => {
+  form.addEventListener("submit", e => {
     e.preventDefault();
     dataCollection();
   });
@@ -99,12 +100,10 @@ function eventxListener() {
       }
     }
     formData["options"] = Alloptions;
-    // console.log(formData);
     let id = questionId.value;
     let questions;
     if (check) {
       if (id == 0) {
-        console.log(formData);
         var result = await sendData(
           "http://ec2-13-232-39-98.ap-south-1.compute.amazonaws.com:3000/admin/questions", //POST Add new question
           formData,
@@ -148,7 +147,7 @@ function eventxListener() {
     check = true;
     questionId.value = 0;
   };
-  moduleContainerCard.addEventListener("click", async function (event) {
+  moduleContainerCard.addEventListener("click", async function(event) {
     event.preventDefault();
     const editDatamodule = event.target.parentElement.parentElement.querySelectorAll(
       ".editDatamodule"
@@ -160,7 +159,7 @@ function eventxListener() {
     } else if (event.target.classList.contains("delete-module")) {
       const moduleCourse = {
         module: editDatamodule[0].innerHTML.trim(),
-        course: editDatamodule[1].innerHTML.trim(),
+        course: editDatamodule[1].innerHTML.trim()
       };
       const message = await deleteData(
         "http://ec2-13-232-39-98.ap-south-1.compute.amazonaws.com:3000/admin/courses", //Module Delelete
@@ -174,7 +173,7 @@ function eventxListener() {
     } else if (event.target.classList.contains("showAll")) {
       const moduleCourse = {
         module: editDatamodule[0].innerHTML.trim(),
-        course: editDatamodule[1].innerHTML.trim(),
+        course: editDatamodule[1].innerHTML.trim()
       };
       AllQuestionofModule = await sendData(
         "http://ec2-13-232-39-98.ap-south-1.compute.amazonaws.com:3000/admin/get-questions",
@@ -187,7 +186,7 @@ function eventxListener() {
       ui.addListOffetchData(AllQuestionofModule);
     }
   });
-  questionList.addEventListener("click", async function (event) {
+  questionList.addEventListener("click", async function(event) {
     event.preventDefault();
     if (event.target.classList.contains("delete")) {
       let _id = event.target.dataset.id;
@@ -220,10 +219,6 @@ function eventxListener() {
           length++;
         }
       }
-
-      for (let i = 0; i < editData.length; i++) {
-        console.log(editData[i].innerHTML.trim() + " " + i);
-      }
       questionInput.value = editData[0].innerHTML.trim();
       let i = 0;
       for (i = 0; i < optionsInput.length; i++) {
@@ -250,7 +245,7 @@ function eventxListener() {
   });
 }
 function UI() {}
-UI.prototype.Green = function (text) {
+UI.prototype.Green = function(text) {
   feedback.classList.add("showItem");
   feedback.classList.add("color");
   feedback.textContent = `${text}`;
@@ -260,7 +255,7 @@ UI.prototype.Green = function (text) {
     feedback.classList.remove("color");
   }, 4000);
 };
-UI.prototype.Red = function (text) {
+UI.prototype.Red = function(text) {
   feedback.classList.add("showItem");
   feedback.textContent = `${text}`;
   check = false;
@@ -268,7 +263,7 @@ UI.prototype.Red = function (text) {
     feedback.classList.remove("showItem");
   }, 4000);
 };
-UI.prototype.addListOffetchData = function (AllQuestionofModule) {
+UI.prototype.addListOffetchData = function(AllQuestionofModule) {
   questionnum = -1;
   questionList.innerHTML = "";
   for (let i = 0; i < AllQuestionofModule.length; i++) {
@@ -292,22 +287,22 @@ UI.prototype.addListOffetchData = function (AllQuestionofModule) {
     this.displayNewOptions(question.options, ele[questionnum]);
   }
 };
-UI.prototype.showQuestion = function (e) {
+UI.prototype.showQuestion = function(e) {
   e.classList.add("show");
 };
-UI.prototype.hideQuestion = function (e) {
+UI.prototype.hideQuestion = function(e) {
   e.classList.remove("show");
 };
-UI.prototype.formFeild = function (inputs, value) {
+UI.prototype.formFeild = function(inputs, value) {
   for (let i = 0; i < inputs.elements.length - 1; i++) {
     inputs.elements[i].value = value;
   }
 };
-UI.prototype.deleteOption = function (num) {
+UI.prototype.deleteOption = function(num) {
   let list = document.querySelector(`.op-${num}`);
   list.parentNode.removeChild(list);
 };
-UI.prototype.addOption = function (element, num) {
+UI.prototype.addOption = function(element, num) {
   const div = document.createElement("div");
   div.classList.add(`op-${num}`);
   div.innerHTML = `<label for="option-1" class="question-label">option ${num} </label>
@@ -319,7 +314,14 @@ UI.prototype.addOption = function (element, num) {
               ></textarea>`;
   element.appendChild(div);
 };
-UI.prototype.addModule = function (element, data) {
+UI.prototype.addMultipleModule = function(data) {
+  moduleContainerCard.innerHTML = "";
+  for (let i = 0; i < data.length; i++) {
+    const value = { CourseName: data[i].course, ModuleName: data[i].module };
+    this.addModule(moduleContainerCard, value);
+  }
+};
+UI.prototype.addModule = function(element, data) {
   const div = document.createElement("div");
   div.classList.add("moduleCard");
   div.innerHTML = `<h1>Module Name</h1> 
@@ -333,13 +335,12 @@ UI.prototype.addModule = function (element, data) {
         </div>`;
   element.appendChild(div);
 };
-UI.prototype.displayNewOptions = function (options, element) {
+UI.prototype.displayNewOptions = function(options, element) {
   const keyArray = Object.values(options);
   const div = document.createElement("div");
   div.classList.add("display-option");
   for (let i = 0; i < keyArray.length; i++) {
     let value = keyArray[i];
-    // console.log(value);
     let optionHtml = `<div>
     <h4>option ${i + 1}</h4>
     <p class="editData">
@@ -350,7 +351,7 @@ UI.prototype.displayNewOptions = function (options, element) {
   }
   element.appendChild(div);
 };
-UI.prototype.addQuestion = function (element, question) {
+UI.prototype.addQuestion = function(element, question) {
   const div = document.createElement("div");
   div.classList.add("card2");
   div.innerHTML = `<h2>Question</h2>
@@ -404,16 +405,18 @@ function Question(
   this.NumberOfBalls = NumberOfBalls;
   this.speed = speed;
 }
-document.addEventListener("DOMContentLoaded", async function () {
+document.addEventListener("DOMContentLoaded", async function() {
+  const ui = new UI();
   const data = await fetchAllData(
     "http://ec2-13-232-39-98.ap-south-1.compute.amazonaws.com:3000/admin/questions"
   );
+  ui.addListOffetchData(data.questions);
   const courses = await fetchAllData(
     "http://ec2-13-232-39-98.ap-south-1.compute.amazonaws.com:3000/admin/courses"
   );
+  ui.addMultipleModule(courses);
   eventxListener();
-  // numOfQuestion.innerHTML = `<h1>Questions:${data.length}</h1>`;
-  // displayQuestionCard(data);
+  numOfQuestion.innerHTML = `<h1>Questions:${data.number}</h1>`;
 });
 
 const sendData = async (url, data, method) => {
@@ -423,11 +426,11 @@ const sendData = async (url, data, method) => {
       method: method,
       body: JSON.stringify(data),
       headers: {
-        "Content-type": "application/json",
-      },
+        "Content-type": "application/json"
+      }
     });
     if (res.status !== 200 && res.status !== 201) {
-      throw new Error("Failed");
+      throw new Error("Failed to send Data");
     }
     res = await res.json();
     return res;
@@ -443,8 +446,8 @@ const deleteData = async (url, _id) => {
       method: "DELETE",
       body: JSON.stringify({ _id }),
       headers: {
-        "Content-type": "application/json",
-      },
+        "Content-type": "application/json"
+      }
     });
     if (res.status !== 200 && res.status !== 201) {
       throw new Error("Failed to delete");
@@ -457,11 +460,10 @@ const deleteData = async (url, _id) => {
   }
 };
 
-const fetchAllData = async (url) => {
+const fetchAllData = async url => {
   try {
     let response = await fetch(url);
     response = await response.json();
-    console.log(response);
     return response;
   } catch (err) {
     const ui = new UI();
@@ -469,19 +471,3 @@ const fetchAllData = async (url) => {
   }
 };
 
-// const displayQuestionCard = data => {
-//   const ui = new UI();
-//   let questions;
-//   for (let i = 0; i < data.length; i++) {
-//     questions = new Question(
-//       data[i]._id,
-//       data[i].question,
-//       data[i].solution,
-//       data[i].option1,
-//       data[i].option2,
-//       data[i].option3,
-//       data[i].option4
-//     );
-//     ui.addQuestion(questionList, questions);
-//   }
-// };
